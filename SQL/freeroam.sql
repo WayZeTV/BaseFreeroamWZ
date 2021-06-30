504c4af626228c4a3c60ac4045d1cfb952cf66a9
WayZe <wayzeprofessionnel@gmail.com>
2021-06-30 17:34:18 +0200

    Add files via upload

---
+++ SQL/freeroam.sql
@@ -0,0 +1,198 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : mer. 30 juin 2021 à 17:18
+-- Version du serveur :  10.4.16-MariaDB
+-- Version de PHP : 7.4.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `freeroam`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `items`
+--
+
+CREATE TABLE `items` (
+  `name` varchar(50) NOT NULL,
+  `label` varchar(50) NOT NULL,
+  `limit` int(11) NOT NULL DEFAULT -1,
+  `rare` int(11) NOT NULL DEFAULT 0,
+  `can_remove` int(11) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `jobs`
+--
+
+CREATE TABLE `jobs` (
+  `name` varchar(50) NOT NULL,
+  `label` varchar(50) DEFAULT NULL,
+  `SecondaryJob` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `jobs`
+--
+
+INSERT INTO `jobs` (`name`, `label`, `SecondaryJob`) VALUES
+('unemployed', 'Unemployed', 0),
+('unemployed2', 'Sans emplois', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `job_grades`
+--
+
+CREATE TABLE `job_grades` (
+  `id` int(11) NOT NULL,
+  `job_name` varchar(50) DEFAULT NULL,
+  `grade` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `label` varchar(50) NOT NULL,
+  `salary` int(11) NOT NULL,
+  `skin_male` longtext NOT NULL,
+  `skin_female` longtext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `job_grades`
+--
+
+INSERT INTO `job_grades` (`id`, `job_name`, `grade`, `name`, `label`, `salary`, `skin_male`, `skin_female`) VALUES
+(1, 'unemployed', 0, 'unemployed', 'Unemployed', 200, '{}', '{}'),
+(2, 'unemployed2', 0, 'rsa', 'Secondaire', 50, '{}', '{}');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `identifier` varchar(50) COLLATE utf8mb4_bin NOT NULL,
+  `license` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL,
+  `money` int(11) DEFAULT NULL,
+  `name` varchar(50) COLLATE utf8mb4_bin DEFAULT '',
+  `skin` longtext COLLATE utf8mb4_bin DEFAULT NULL,
+  `job` varchar(50) COLLATE utf8mb4_bin DEFAULT 'unemployed',
+  `job_grade` int(11) DEFAULT 0,
+  `job2` varchar(255) COLLATE utf8mb4_bin DEFAULT 'unemployed2',
+  `job2_grade` int(11) DEFAULT 0,
+  `loadout` longtext COLLATE utf8mb4_bin DEFAULT NULL,
+  `position` varchar(36) COLLATE utf8mb4_bin DEFAULT NULL,
+  `bank` int(11) DEFAULT NULL,
+  `permission_level` int(11) DEFAULT NULL,
+  `group` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL,
+  `is_dead` tinyint(1) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user_accounts`
+--
+
+CREATE TABLE `user_accounts` (
+  `id` int(11) NOT NULL,
+  `identifier` varchar(22) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `money` double NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user_inventory`
+--
+
+CREATE TABLE `user_inventory` (
+  `id` int(11) NOT NULL,
+  `identifier` varchar(22) NOT NULL,
+  `item` varchar(50) NOT NULL,
+  `count` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `items`
+--
+ALTER TABLE `items`
+  ADD PRIMARY KEY (`name`);
+
+--
+-- Index pour la table `jobs`
+--
+ALTER TABLE `jobs`
+  ADD PRIMARY KEY (`name`);
+
+--
+-- Index pour la table `job_grades`
+--
+ALTER TABLE `job_grades`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`identifier`);
+
+--
+-- Index pour la table `user_accounts`
+--
+ALTER TABLE `user_accounts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `user_inventory`
+--
+ALTER TABLE `user_inventory`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `job_grades`
+--
+ALTER TABLE `job_grades`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT pour la table `user_accounts`
+--
+ALTER TABLE `user_accounts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT pour la table `user_inventory`
+--
+ALTER TABLE `user_inventory`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ start.bat
@@ -0,0 +1,3 @@
+RMDIR /s /q "D:\Freeroam\cache"
+cd /d D:\Freeroam
+D:\Freeroam\FXServer.exe +exec server.cfg
